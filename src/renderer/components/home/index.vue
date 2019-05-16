b9ef9b6fba75247dc5b6ffd52658781cc8e19868
xiaodashi12 <1207688613@qq.com>
2019-05-16 16:51:22 +0800

    测试代码

---
+++ src/renderer/components/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="form-body">
+        <span>
+            hello-electron
+        </span>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+    .form-body{
+        span{
+            color:red;
+        }
+    }
+</style>
